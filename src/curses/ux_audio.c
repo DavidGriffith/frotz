DavidGriffith/frotz

commit 09485e3a5b64b1609f0ccfbb043d309bb5b8868f

No more segfaults when playing an AIFF when an OGG is already playing. Sound of any kind is distorted after this.

src/curses/ux_audio.c
@@ -538,8 +538,8 @@ void *playaiff(EFFECT *raw_effect)
 
 	/* If that's all, terminate and signal that we're done. */
 	if (src_data.end_of_input && src_data.output_frames_gen == 0) {
-	    pthread_mutex_unlock(&mutex);
 	    sem_post(&audio_full);
+	    pthread_mutex_unlock(&mutex);
 	    break;
 	}
 
@@ -559,8 +559,10 @@ void *playaiff(EFFECT *raw_effect)
      */
 
     bleep_playing = FALSE;
+    memset(bleepbuffer, 0, BUFFSIZE * sizeof(float) * 2);
+
     pthread_mutex_unlock(&mutex);
-//    sem_post(&audio_empty);
+    sem_post(&audio_empty);
 
     fseek(myeffect.fp, filestart, SEEK_SET);
     sf_close(sndfile);
@@ -649,7 +651,7 @@ static void *playmod(EFFECT *raw_effect)
     if (myeffect.vol > 8) myeffect.vol = 8;
     ModPlug_SetMasterVolume(mod, mypower(2, myeffect.vol));
 
-    shortbuffer = malloc(BUFFSIZE * sizeof(char) * 2);
+    shortbuffer = malloc(BUFFSIZE * sizeof(short) * 2);
 
     music_playing = TRUE;
 

commit e9b77341a57ea636ea1dcaf288469e5f195fdd0f

09485e3a caused subsequent bleeps to be distorted.  Segfaults back again.

src/curses/ux_audio.c
@@ -573,7 +573,7 @@ void *playaiff(EFFECT *raw_effect)
 //    fseek(myeffect.fp, filestart, SEEK_SET);
 
     pthread_mutex_unlock(&mutex);
-    sem_post(&audio_empty);
+//    sem_post(&audio_empty);
 
     sf_close(sndfile);
     free(floatbuffer);
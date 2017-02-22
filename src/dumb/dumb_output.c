a90a40bd75f0cd0ed97be9661864c3bd8650d6d0
David Griffith <dave@661.org>
2017-02-22 05:10:08 -0800

    Added GPL and FSF stuff to leading comments in src/dumb/

--- src/dumb/dumb_output.c
+++ src/dumb/dumb_output.c
@@ -1,7 +1,22 @@
-/* dumb-output.c
+/*
+ * dumb_output.c - Dumb interface, output functions
  *
- * Copyright 1997,1998 Alfresco Petrofsky <alfresco@petrofsky.berkeley.edu>.
- * Any use permitted provided this notice stays intact.
+ * This file is part of Frotz.
+ *
+ * Frotz is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Frotz is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * Or visit http://www.fsf.org/
  */
 
 #include "dumb_frotz.h"
--- src/dumb/dumb_pic.c
+++ src/dumb/dumb_pic.c
@@ -1,9 +1,24 @@
-/* dumb-pic.c
+/*
+ * dumb-pic.c - Dumb interface, picture outline functions
  *
- * Copyright 1997,1998 Alcibiades Petrofsky
- * <alcibiades@petrofsky.berkeley.ca.us>.
- * Any use permitted provided this notice stays intact.
+ * This file is part of Frotz.
+ *
+ * Frotz is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Frotz is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * Or visit http://www.fsf.org/
  */
+
 #include "dumb_frotz.h"
 
 f_setup_t f_setup;
